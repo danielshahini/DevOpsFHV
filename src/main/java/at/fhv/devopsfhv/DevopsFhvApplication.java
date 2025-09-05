cab6e6c404953d1273c2e5f3f422e92e3e3f92c5
Niklas <niklas.a.rist@gmail.com>
2025-09-05 10:03:25 +0200

    + Setup

---
+++ src/main/java/at/fhv/devopsfhv/DevopsFhvApplication.java
@@ -0,0 +1,13 @@
+package at.fhv.devopsfhv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsFhvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsFhvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhv/devopsfhv/DevopsFhvApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhv.devopsfhv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsFhvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
